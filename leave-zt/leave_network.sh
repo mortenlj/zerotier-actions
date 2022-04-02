1a0e0d9b26ad7e17201d99a28c9dd99a57cec523
Morten Lied Johansen <mortenjo@ifi.uio.no>
2022-04-02 22:46:42 +0200

    Make sure to delete node before leaving

--- leave-zt/leave_network.sh
+++ leave-zt/leave_network.sh
@@ -4,6 +4,16 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # abort on pipe failures
 
+echo "Deleting node"
+
+url="${API_URL}/api/v1/network/${NETWORK_ID}/member/${NODE_ID}"
+
+curl --oauth2-bearer "${API_ACCESSTOKEN}" \
+     --output /dev/null \
+     --silent \
+     --show-error \
+     --request DELETE \
+     "${url}"
+
 echo "Leaving network"
 sudo zerotier-cli leave "${NETWORK_ID}"
-
