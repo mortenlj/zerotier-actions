82cc6dc3f693d9b2ead9d7e1eef3febfd16f57b7
Morten Lied Johansen <mortenjo@ifi.uio.no>
2022-04-02 21:45:17 +0200

    Trigger workflow

--- test_connectivity.sh
+++ test_connectivity.sh
@@ -5,5 +5,4 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # abort on pipe failures
 
 echo "Testing connection to ZeroTier network, by pinging a connected host"
-
 ping -c 10.255.255.34
