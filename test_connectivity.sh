9d245843b2befdc16d91e92ff7a6c321309385b4
Morten Lied Johansen <mortenjo@ifi.uio.no>
2023-08-30 11:27:44 +0200

    Update test with better targets

--- test_connectivity.sh
+++ test_connectivity.sh
@@ -5,4 +5,4 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # abort on pipe failures
 
 echo "Testing connection to ZeroTier network, by pinging a connected host"
-ping -c 5 10.255.255.34
+ping -c 5 emrys.home.zt.ibidem.no
