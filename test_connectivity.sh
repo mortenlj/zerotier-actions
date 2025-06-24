713af5d111ca1f68d1fe6c0817a916b0a910c5f8
Morten Lied Johansen <mortenjo@ifi.uio.no>
2025-06-24 11:03:48 +0200

    Emrys was retired ages ago

--- test_connectivity.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-set -o errexit   # abort on nonzero exitstatus
-set -o nounset   # abort on unbound variable
-set -o pipefail  # abort on pipe failures
-
-echo "Testing connection to ZeroTier network, by pinging a connected host"
-ping -c 5 emrys.home.zt.ibidem.no
